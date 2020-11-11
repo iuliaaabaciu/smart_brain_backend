cbc36acb94c5c8dc809f40f9a8aa373477f19ed8
iuliab <iuliaiuliabaciu@yahoo.com>
2020-11-11 19:04:30 +0200

    added db connection

--- api/server.js
+++ api/server.js
@@ -11,10 +11,8 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : 'postgresql-cubed-65778',
-    user : 'postgres',
-    password : 'toto',
-    database : 'smartbrain'
+    host : process.env.DATABASE_URL,
+    ssl: true,
   }
 });
 
